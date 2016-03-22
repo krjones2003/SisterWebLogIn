6267dda4e158933d37755fc9d23572fe560e80bb
Almudena <akonradd@mills.edu>
2016-03-22 23:49:17 +0000

    visual overhaul

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
+
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 gem 'protected_attributes'
 gem 'annotate', '~> 2.4.1.beta'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get 'staticpages/home'
 
   get 'staticpages/profiles'
+  
+  get 'staticpages/about'
 
   get 'staticpages/calendar'
+  
 
   resources :profiles
   root :to => redirect('/staticpages/home')
