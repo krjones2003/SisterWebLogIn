8da05395b7485e2a216c17ddef1431a8177b3f20
Kristin <kjones@mills.edu>
2016-04-01 13:11:31 +0000

    added events seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,15 @@ more_profiles = [
 more_profiles.each do |profiles|
   Profile.create!(profiles)
 end
+
+
+more_events = [
+  {:what => 'Scrum', :where => 'Mills College',
+    :when => '29/4/2016'},
+  {:what => 'Interview Prep', :where => 'Mills College',
+   :when => '15/4/2016'}
+]
+
+more_events.each do |events|
+  Event.create!(events)
+end
